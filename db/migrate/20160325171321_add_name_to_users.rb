5a0f0bfdf996f87f2ac0e72cc8e9b98050a76c9a
Matthew Cliatt <mtcliatt@valdosta.edu>
2016-03-25 13:20:54 -0400

    basic user generated

---
+++ db/migrate/20160325171321_add_name_to_users.rb
@@ -0,0 +1,8 @@
+class AddNameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :age, :integer
+    add_column :users, :username, :string
+    add_column :users, :password, :string
+  end
+end
