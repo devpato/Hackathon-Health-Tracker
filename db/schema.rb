c1888fc7910190bde14337a63e16ce18d5db8688
Pato <pevargas@valdosta.edu>
2016-03-26 10:07:03 -0400

    foods scaffold front end

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326131605) do
+ActiveRecord::Schema.define(version: 20160326135507) do
 
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20160326131605) do
     t.integer  "sugar"
     t.string   "location"
     t.string   "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "goals", force: :cascade do |t|
+    t.integer  "type_id"
+    t.string   "title"
+    t.integer  "end"
+    t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
